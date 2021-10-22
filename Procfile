c730ecb8b112b567579dc103875d4cbd6a2618c1
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 18:27:29 -0500

    Testing over redistogo heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-heroku addons:create redistogo
 web: uvicorn sabrina_date:app --host=0.0.0.0 --port=${PORT:-5000}
+worker: python worker.py
--- worker.py
+++ worker.py
@@ -1,14 +1,12 @@
 import os
 import urllib.parse as urlparse
-from redis import Redis
+import aioredis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL')
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
-conn = Redis(Host=url.hostname, port=url.port, db=0, password=url.password)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+conn = await aioredis.Redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
