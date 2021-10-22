949865a118516118bb799bc8b6731b0aefb8f21b
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 17:55:27 -0500

    Update worker

--- worker.py
+++ worker.py
@@ -1,14 +1,11 @@
 import os
-import urlparse
+from urllib.parse import urlparse
 from redis import Redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL')
-if not redis_url:
-    raise RuntimeError('Set up Redis To Go first.')
-
 urlparse.uses_netloc.append('redis')
 url = urlparse.urlparse(redis_url)
 conn = Redis(Host=url.hostname, port=url.port, db=0, password=url.password)
