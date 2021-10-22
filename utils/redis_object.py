8428e757cfe84bba15e2c07cbf8faaa9bf493947
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 18:08:15 -0500

    Update redis object

--- utils/redis_object.py
+++ utils/redis_object.py
@@ -6,15 +6,12 @@ import urllib.parse as urlparse
 # redis = None
 HEROKU = True
 TEST_REDIS_URL = 'redis://localhost'
-HEROKU_REDIS_URL = os.getenv('REDISTOGO_URL')
-
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
+HEROKU_REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 
 async def redis_connection():
     if HEROKU:
-        redis_db = await aioredis.Redis(Host=url.hostname, port=url.port, db=0, password=url.password)
+        redis_db = await aioredis.Redis.from_url(HEROKU_REDIS_URL)
     else:
-        pass
+        redis_db = await aioredis.Redis.from_url(TEST_REDIS_URL)
     return redis_db
