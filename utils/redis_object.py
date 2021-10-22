801a1117ea1ec8007e640e86d2f8c72b85bd01ff
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 17:37:17 -0500

    Update utils for redis object

--- utils/redis_object.py
+++ utils/redis_object.py
@@ -7,7 +7,10 @@ import os
 TEST_REDIS_URL = 'redis://localhost'
 HEROKU_REDIS_URL = os.getenv('REDISTOGO_URL')
 
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+
 
 async def redis_connection():
-    redis_db = await aioredis.Redis.from_url(HEROKU_REDIS_URL)
+    redis_db = await aioredis.Redis(Host=url.hostname, port=url.port, db=0, password=url.password)
     return redis_db
