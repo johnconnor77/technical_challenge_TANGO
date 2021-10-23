2c42182643a8a1bafb84a1658d810da02d4c3246
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 20:14:58 -0500

    Update on redis_URL

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-conn = redis.from_url("redis: //redis-12025.c291.ap-southeast-2-1.ec2.cloud.redislabs.com:12025")
+conn = redis.from_url("redis://:p6d6c8a0c7ca8b15cbbe57a480c9e749f412a23a75de2948a32f50d6b246e5f9b@ec2-184-73-83-60.compute-1.amazonaws.com:14380")
 
 if __name__ == '__main__':
     with Connection(conn):
