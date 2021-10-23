5f6a73bcbd79e2b87859c780e83b0f98e50ec062
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 19:55:49 -0500

    Update with remote redis usage

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: uvicorn sabrina_date:app --host=0.0.0.0 --port=${PORT:-5000}
+worker: python worker.py
--- worker.py
+++ worker.py
@@ -5,8 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
+conn = redis.from_url("redis: //redis-12025.c291.ap-southeast-2-1.ec2.cloud.redislabs.com:12025")
 
 if __name__ == '__main__':
     with Connection(conn):
