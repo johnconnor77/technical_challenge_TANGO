6ef2b4d76bbbc29cb96984c264a392d2d9794a89
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 20:21:25 -0500

    Update redis url

--- utils/redis_object.py
+++ utils/redis_object.py
@@ -6,8 +6,7 @@ import urllib.parse as urlparse
 # redis = None
 HEROKU = True
 TEST_REDIS_URL = 'redis://localhost'
-HEROKU_REDIS_URL = "redis://:p6d6c8a0c7ca8b15cbbe57a480c9e749f412a23a75de2948a32f50d6b246e5f9b@ec2-184-73-83-60.compute-1.amazonaws.com:14380"
-
+HEROKU_REDIS_URL = "redis-12007.c100.us-east-1-4.ec2.cloud.redislabs.com:12007"
 
 def redis_connection():
     if HEROKU:
--- worker.py
+++ worker.py
@@ -5,7 +5,8 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-conn = redis.from_url("redis://:p6d6c8a0c7ca8b15cbbe57a480c9e749f412a23a75de2948a32f50d6b246e5f9b@ec2-184-73-83-60.compute-1.amazonaws.com:14380")
+redis_url = "redis-12007.c100.us-east-1-4.ec2.cloud.redislabs.com:12007"
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
