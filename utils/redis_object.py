9148d3f9e2dfe944a0fe84e462e67094c9152f21
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 19:25:03 -0500

    Update con redis heroku

--- utils/redis_object.py
+++ utils/redis_object.py
@@ -6,12 +6,15 @@ import urllib.parse as urlparse
 # redis = None
 HEROKU = True
 TEST_REDIS_URL = 'redis://localhost'
-HEROKU_REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+#HEROKU_REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 
 def redis_connection():
     if HEROKU:
-        redis_db = redis.from_url(HEROKU_REDIS_URL)
+        redis_url = os.getenv('REDISTOGO_URL')
+        urlparse.uses_netloc.append('redis')
+        url = urlparse.urlparse(redis_url)
+        redis_db = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
     else:
         redis_db = redis.from_url(TEST_REDIS_URL)
     return redis_db
