ee3c5480934089dfc457e7f0c23b2abed4533005
johnconnor77 <jfcalleh@unal.edu.co>
2021-10-22 19:48:36 -0500

    Update for remote access with redis database

--- utils/redis_object.py
+++ utils/redis_object.py
@@ -6,15 +6,12 @@ import urllib.parse as urlparse
 # redis = None
 HEROKU = True
 TEST_REDIS_URL = 'redis://localhost'
-#HEROKU_REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+HEROKU_REDIS_URL = "redis: //redis-12025.c291.ap-southeast-2-1.ec2.cloud.redislabs.com:12025"
 
 
 def redis_connection():
     if HEROKU:
-        redis_url = os.getenv('REDISTOGO_URL')
-        urlparse.uses_netloc.append('redis')
-        url = urlparse.urlparse(redis_url)
-        redis_db = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+        redis_db = redis.from_url(HEROKU_REDIS_URL)
     else:
         redis_db = redis.from_url(TEST_REDIS_URL)
     return redis_db
